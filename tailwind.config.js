d84235ccd26d132404c85d82256cbbe55b968026
Nikhil Dhanda <nikhildhanda84@gmail.com>
2024-12-07 16:46:47 +0530

    modified: fixed vercel erroe

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 const textShadowPlugin = require('tailwindcss-textshadow');
-export default {
+
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
     extend: {
       textShadow: {
-        custom: '0 4px 4px rgba(0, 0, 0, 0.25)', 
+        custom: '0 4px 4px rgba(0, 0, 0, 0.25)',
       },
     },
   },
   plugins: [
     textShadowPlugin,
   ],
-}
+};
